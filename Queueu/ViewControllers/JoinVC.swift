0a8fc6a16fadff4ccecf0f00507a546e9dbfbfa4
Charlotte Ristiniemi <charlotte@ristiniemi.se>
2018-10-10 16:55:07 +0200

    first controllers created

---
+++ Queueu/ViewControllers/JoinVC.swift
@@ -0,0 +1,33 @@
+//
+//  JoinVC.swift
+//  Queueu
+//
+//  Created by Charlotte Ristiniemi on 2018-10-10.
+//  Copyright © 2018 Charlotte Ristiniemi. All rights reserved.
+//
+
+import UIKit
+
+class JoinVC: UIViewController {
+
+  // MARK: Properties
+  @IBOutlet weak var textFieldName: UITextField!
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
---
+++ Queueu/ViewControllers/QueueVC.swift
@@ -0,0 +1,31 @@
+//
+//  QueueVC.swift
+//  Queueu
+//
+//  Created by Charlotte Ristiniemi on 2018-10-10.
+//  Copyright © 2018 Charlotte Ristiniemi. All rights reserved.
+//
+
+import UIKit
+
+class QueueVC: UIViewController {
+
+  @IBOutlet weak var navBar: UINavigationBar!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+  
+
+  /*
+  // MARK: - Navigation
+
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // Get the new view controller using segue.destination.
+    // Pass the selected object to the new view controller.
+  }
+  */
+
+}
